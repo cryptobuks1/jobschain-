fd041e239134a4f31bf9491f33911d5681cb82e7
Na-ser <nassermarisha@gmail.com>
2019-08-12 04:52:03 -0700

    cvs

--- app/Http/Controllers/CVsController.php
+++ app/Http/Controllers/CVsController.php
@@ -1,88 +1,60 @@
 <?php
-/**Envatic Crypto AP
-* Adapted by
- *Stephen Isaac:  ofuzak@gmail.com>.
- *Skype: ofuzak
- *www.evatic.com (Deploy Scripts , Apps , coins in One click)
- */
-	
+
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
-use App\Models\Cv;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\URL;
-use Yajra\Datatables\Datatables;
-
 
 class CvsController extends Controller
 {
-	
-	
     /**
-     * Display a listing of the resource. (uses ajax table)
+     * Display a listing of the resource.
      *
-     * @return \Illuminate\View\View
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-      $cvs = Cv::paginate(3);
-		return view('cvs.index',compact('cvs'));
+        //
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\View\View
+     * @return \Illuminate\Http\Response
      */
     public function create()
-    {
-       return view('cvs.create');
+    { 
+        return view('mainpage.postcv');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Cv $cv)
+    public function store(Request $request)
     {
-  
-      $cv->address = $request->address;
-      $cv->description=$request->description;	
-      $cv->location=$request->location;
-      $cv->salary=$request->salary;
-
-      $cv->save();
-		return response()->json(['status' => 'SUCCESS','message' => __('app.action_ok',['item'=>'Cv','action'=> __('app.added')])]);
-
+        //
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     *
-     * @return \Illuminate\View\View
+     * @return \Illuminate\Http\Response
      */
-    public function show(Cv $cv)
+    public function show($id)
     {
-		
-       // 
+        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     *
-     * @return \Illuminate\View\View
+     * @return \Illuminate\Http\Response
      */
-    public function edit(Request $request, Cv $cv)
+    public function edit($id)
     {
         //
     }
@@ -90,69 +62,23 @@ class CvsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Cv $cv)
+    public function update(Request $request, $id)
     {
-		//
+        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(Request $request, Cv $cv)
+    public function destroy($id)
     {
-		//
+        //
     }
-	
-	  /**toggle Item status.
-     *
-     * @param  int  $id
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function toggle_status(Request $request, Cv $cv)
-    {
-		//
-    }
-	
-	/**
-     * Remove the specified resources from storage.
-     *
-     * @param  Request  $request
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function delete(Request $request)
-    {
-		
-		//
-    }
-	
-	  /**toggle Item status.
-     *
-     * @param  int  $id
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function toggle_statuses(Request $request)
-    {
-		//
-    }
-	
-	/**
-     * Get the Table.
-     *
-     * 
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-
 }
